9338cc23f2b9a4286f9b0f6538ee1af2e42c16f7
bakabaka.bunbun <54648150+bakabun@users.noreply.github.com>
2022-07-24 09:03:32 +0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.Interactivity;
+using DSharpPlus.Interactivity.Extensions;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+using var host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices((ctx, svc) => svc
+        .AddSingleton(new DiscordClient(new DiscordConfiguration
+        {
+            Token = ctx.Configuration["BotToken"],
+            TokenType = TokenType.Bot,
+            AutoReconnect = true,
+            ReconnectIndefinitely = true,
+            MinimumLogLevel = LogLevel.Debug
+        }))
+        .AddSingleton<HttpClient>())
+    // .UseEnvironment("Development")
+    .Build();
+
+var client = host.Services.GetRequiredService<DiscordClient>();
+
+var cnext = client.UseCommandsNext(new CommandsNextConfiguration
+{
+    EnableMentionPrefix = true,
+    Services = host.Services
+});
+
+client.UseInteractivity(new InteractivityConfiguration
+{
+    AckPaginationButtons = true
+});
+
+await client.ConnectAsync();
+await host.RunAsync();
---
+++ README.md
@@ -0,0 +1,9 @@
+# Proxima2
+
+## Features
+
+TBD
+
+## Note
+
+- Don't push bot token to repo, baka
